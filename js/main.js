aca78d42663c884085cab7dc87af1ef8475752de
Jhon Tejada <jhontejada13@gmail.com>
2019-12-19 22:41:51 -0500

    Añadimos lineas para animación de abrir formulario

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-const tarjeta = document.querySelector('#tarjeta');
+const tarjeta = document.querySelector('#tarjeta'),
+      btnAbrirFormulario = document.querySelector('#btn-abrir-formulario'),
+      formulario = document.querySelector('#formulario-tarjeta');
 
 /*const mostrarFrente = () => {
     if(tarjeta.classList.contains('active')){
@@ -8,4 +10,9 @@ const tarjeta = document.querySelector('#tarjeta');
 
 tarjeta.addEventListener('click', () => {
     tarjeta.classList.toggle('active');
+});
+
+btnAbrirFormulario.addEventListener('click', () => {
+    btnAbrirFormulario.classList.toggle('active');
+    formulario.classList.toggle('active');
 });
